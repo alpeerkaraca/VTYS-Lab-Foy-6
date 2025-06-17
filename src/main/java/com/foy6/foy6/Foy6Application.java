28298837f0e4f7237fd3bcffc0047c037cd78632
Alper Karaca <alpeerkaraca@gmail.com>
2025-06-17 04:54:34 +0300

    Initial Commit

---
+++ src/main/java/com/foy6/foy6/Foy6Application.java
@@ -0,0 +1,13 @@
+package com.foy6.foy6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Foy6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Foy6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/foy6/foy6/Foy6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foy6.foy6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Foy6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
